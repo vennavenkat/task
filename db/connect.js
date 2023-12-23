30b9bd481c6681e76a201cf64e64c4996fb4aa0c
vvenna <vvenna@axiphyl.com>
2023-12-23 14:09:55 -0600

    Intial Commit

---
+++ db/connect.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+
+
+const connectDB = (url) =>{
+    return mongoose
+      .connect(url,
+         {  useNewUrlParser: true,
+            useUnifiedTopology: true 
+        })
+      .then(() => console.log('CONNECTED TO THE DB...'))
+      .catch((err) => console.log(err));
+}
+
+module.exports = connectDB
